91670c15793d231f81f4c3e3fd9707341ef077a5
Jaimeibarrae <49220342+Jaimeibarrae@users.noreply.github.com>
2020-02-19 22:16:48 -0600

    Update JACI_mayor_menor.py

--- JACI_mayor_menor.py
+++ JACI_mayor_menor.py
@@ -1,22 +1,40 @@
-def main():
-    print("MAYOR y MENOR")
-    numero = int(input("¿Cuántos valores va a introducir? "))
-
-    if numero <= 0:
-        print("No se puede comparar amigo")
+def mayor(n1,n2):
+    
+    if(n1>n2):
+        print("El numero mayor es: ",n1)
+    elif(n1<n2):
+        print("El numero mayor es: ",n2)
     else:
-        valor = float(input("Escriba el número 1: "))
-        minimo = maximo = suma = valor
-        for i in range(2, numero + 1):
-            valor = float(input(f"Escriba el número {i}: "))
-            suma = suma + valor
-            if valor < minimo:
-                minimo = valor
-            if valor > maximo:
-                maximo = valor
-        print(f"El número menor es: {minimo}")
-        print(f"El número mayor es: {maximo}")
-      
+        print("Hay algun error con tus números")
 
-if __name__ == "__main__":
-    main()
+      
+def menor(n1,n2):
+    
+    if(n1>n2):
+        print("El numero menor es: ",n2)
+    elif(n1<n2):
+        print("El numero menor es: ",n1)
+    else:
+        print("Hay algun error con tus números")
+
+def menu(funcion):
+    if(funcion=='1'):
+        print("-------MAYOR--------")
+        n1=input("Ingrese el primer numero a comprar: ")
+        n2=input("Ingrese el segundo numero a comprar: ")
+        mayor(n1,n2)
+    elif (funcion=='2'):    
+        print("-------MENOR--------")    
+        n1=input("Ingrese el primer numero a comprar: ")
+        n2=input("Ingrese el segundo numero a comprar: ")
+        menor(n1,n2)
+    else:
+        print("Algo hiciste mal")
+
+
+print("MENU DE FUNCIONES")
+print("1) Saber mayor")
+print("2) Saber menor\n")
+funcion=input("Ingrese lo que desea saber:\n")
+menu(funcion)
+
