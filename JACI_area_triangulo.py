c45c4ec13cd2b1a359be568209bc61c0c467972b
Jaimeibarrae <49220342+Jaimeibarrae@users.noreply.github.com>
2020-02-19 21:57:53 -0600

    Update JACI_area_triangulo.py

--- JACI_area_triangulo.py
+++ JACI_area_triangulo.py
@@ -1,4 +1,8 @@
-base=int(input("Ingrese la Base\n"))
-altura=int(input("Ingrese la altura\n"))
-area=(base*altura)/2
-print("Esta es el area del tringulo",area)
+def areaT(base,altura):
+    area=(base*altura)/2
+    print("Esta es el area del tringulo",area)
+
+base=int(input("Ingrese la Base:\n"))
+altura=int(input("Ingrese la altura:\n"))
+areaT(base,altura)
+input("")
